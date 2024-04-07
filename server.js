d247151f038823fc5f70ff69af0a4b6edcfc1449
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-04-07 23:37:29 +0530

    new

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const academyRoute = require('./routes/AcademyRoute');
 const playerPostRoute = require('./routes/PlayerPostRoute');
 
 const app = express();
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.header(
@@ -18,7 +19,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors);
+// app.use(cors);
 
 app.use(express.json());
 app.use(cookieParser());
