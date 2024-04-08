ac3cc9a761ded9186caf506f2aa584df6b8f0897
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-04-08 13:03:49 +0530

    new12

--- server.js
+++ server.js
@@ -12,9 +12,19 @@ const playerPostRoute = require('./routes/PlayerPostRoute');
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS options
+const corsOptions = {
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+};
+
+// Enable CORS with options
+app.use(cors(corsOptions));
 
 // Logging middleware
 app.use((req, res, next) => {
