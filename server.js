122b947ee480206e19bffd70aa6a3f6d3a91e333
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-04-08 10:51:00 +0530

    new12

--- server.js
+++ server.js
@@ -10,38 +10,44 @@ const academyRoute = require('./routes/AcademyRoute');
 const playerPostRoute = require('./routes/PlayerPostRoute');
 
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Origin , Accept, X-Requested-With'
-  );
-  next();
-});
-// app.use(cors);
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+// Logging middleware
 app.use((req, res, next) => {
   console.log(req.path, '  ', req.method);
   console.log(req.body);
   next();
 });
 
+// Routes
 app.use('/api/player', playerRoute);
 app.use('/api/coach', coachRoute);
 app.use('/api/academy', academyRoute);
 app.use('/api/playerpost', playerPostRoute);
 
+// CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Origin, Accept, X-Requested-With'
+  );
+  next();
+});
+
+// Database connection and server start
 mongoose
   .connect(process.env.DB_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('connected to db');
+      console.log('Server started on port', process.env.PORT);
+      console.log('Connected to database');
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Error connecting to database:', err);
   });
