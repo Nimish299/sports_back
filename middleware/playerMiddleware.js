e4642e8b61c2ac5b81c45afde0a9962369ac5254
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-04-25 15:51:09 +0530

    local storage middleware

--- middleware/playerMiddleware.js
+++ middleware/playerMiddleware.js
@@ -1,17 +1,34 @@
 const jwt = require('jsonwebtoken');
 
 const playermiddle = (req, res, next) => {
-  const token = req.cookies.playerid;
+  // console.log('fnerurujfb5tyurgvtgv uty');
+  // const token = req.headers.Authorization;
+  // const { Authorization } = req.headers;
+  // console.log('here ', Authorization);
+
+  const token = req.header('auth-tokken');
+  console.log('middle', token);
+
   if (!token) {
     return res.status(401).json({ error: 'no token present' });
   }
+
   jwt.verify(token, process.env.JWT, (err, playerid) => {
     if (err) {
       return res.status(401).json({ error: 'wrong token present' });
     }
+    console.log(playerid);
     req.playerid = playerid.id;
     next();
   });
+
+  // jwt.verify(token, process.env.JWT, (err, playerid) => {
+  //   if (err) {
+  //     return res.status(401).json({ error: 'wrong token present' });
+  //   }
+  //   req.playerid = playerid.id;
+  //   next();
+  // });
 };
 
 module.exports = {
